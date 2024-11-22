b7857d7ca7b88b11f2e3316a2a74dcd689722096
NIKHIL <121725918+nk1710@users.noreply.github.com>
2024-11-23 01:27:41 +0530

    Update List.js

--- List.js
+++ List.js
@@ -1,6 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { fetchUsers, createUser  } from "../api";
-import UserForm from "./User Form";
+import UserForm from "./User Form"; // Ensure the filename matches
 
 const UserList = () => {
-  const [
+  const [users, setUsers] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await fetchUsers();
+      setUsers(data);
+    };
+    getUsers();
+  }, []);
+
+  const handleAddUser  = async (newUser ) => {
+    const createdUser  = await createUser (newUser );
+    setUsers([...users, createdUser ]);
+    setShowForm(false);
+  };
+
+  return (
+    <div>
+      <h2>User Management</h2>
+      <button onClick={() => setShowForm(true)}>Add User</button>
+      {showForm && <User Form onSubmit={handleAddUser } />}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.role} - {user.status}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
